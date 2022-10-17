da9a94239e45c104157a3335e8f97988e856d25f
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-10-17 21:27:08 +0300

    Add files via upload

---
+++ MirrorWords_02.java
@@ -0,0 +1,51 @@
+package ExamPreparationPart1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MirrorWords_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        String regex = "(@|#)(?<first>[A-Za-z]{3,})\\1\\1(?<second>[A-Za-z]{3,})\\1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> pairs = new ArrayList<>();
+        int countAllPairs = 0; //броя на всички двойки
+
+        while (matcher.find()){
+
+            String firstWord = matcher.group("first");
+            String secondWord = matcher.group("second");
+
+            //Проверка дали думите са печелиши
+            StringBuilder secondWordBuilder = new StringBuilder(secondWord);
+            String reversedSecondWord = secondWordBuilder.reverse().toString();
+            countAllPairs++;
+
+            if(firstWord.equals(reversedSecondWord)){
+
+                pairs.add(firstWord + " <=> " + secondWord);
+
+            }
+        }
+
+        if(countAllPairs == 0){
+            System.out.println("No word pairs found!");
+        }else{
+            System.out.println(countAllPairs + " " + "word pairs found!");
+        }
+        if(pairs.size() == 0){
+            System.out.println("No mirror words!");
+        }else{
+            System.out.println("The mirror words are:");
+            System.out.println(String.join(", ", pairs));
+        }
+    }
+}
